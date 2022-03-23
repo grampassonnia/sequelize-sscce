5427f4a8c5c4137cb174a23c63c330435bc7d52a
Filippo Ceron <kaim.aragonar@gmail.com>
2022-03-23 23:02:57 +0100

    Update sscce.js
    follow up Sequelize tutorial, start model basics

--- src/sscce.js
+++ src/sscce.js
@@ -1,4 +1,6 @@
 'use strict';
+// test only for postgres dialect
+if (process.env.DIALECT !== "postgres") return;
 
 // Require the necessary things from Sequelize
 const { Sequelize, Op, Model, DataTypes } = require('sequelize');
@@ -17,15 +19,50 @@ const { expect } = require('chai');
 // Your SSCCE goes inside this function.
 module.exports = async function() {
   const sequelize = createSequelizeInstance({
+    
     logQueryParameters: true,
     benchmark: true,
+    logging: (...msg) => console.log(msg), // Displays all log function call parameters
     define: {
       timestamps: false // For less clutter in the SSCCE
     }
   });
+  
+  / NOTE: internally, sequelize. define calls Model.init 
+  // so it's equivalent to the class extension approach
+  const User = sequelize.define('User', {
+    // Model attributes are defined here
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataType.STRING,
+      // allowNull defualts to true
+    }
+  }, {
+    // other model options go here, like providing the table name directly, etc.
+  });
 
-  const Foo = sequelize.define('Foo', { name: DataTypes.TEXT });
-
+  // `sequelize define` also returns the model
+  console.log(User === sequelize.models.User); // true
+  
+  // This checks what is the current state of the table in the database (which columns
+  // it has, what are their data types, etc), and then performs the necessary changes in
+  // the table to make it match the model
+  User.sync({ alter: true });
+  
+  //DROP a table referred to a model. to run methods on all models (like sync() or drop()) type `sequelize.<methodname>();`
+  //await User.drop();
+  //console.log("User table dropped!");
+  
+  // This will run .sync() only if database name ends with '_test'
+  // sequelize.sync({ force: true, match: /_test$/ });
+  
+  // can be destructive operations. Therefore, they are not recommended for
+  // production-level software. Instead, synchronization should be done with
+  // the advanced concept of Migrations, with the help of the Sequelize CLI.
+  
   const spy = sinon.spy();
   sequelize.afterBulkSync(() => spy());
   await sequelize.sync();
